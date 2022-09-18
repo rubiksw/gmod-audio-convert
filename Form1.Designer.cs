aeac25d4bd970b56ade40b1a7817c77c6e3a6d2e
titsrp_rubik <rubik@titsrp.com>
2022-09-17 23:35:29 -0400

    initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,157 @@
+﻿
+namespace gmod_audio_converter
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.SourcePath = new System.Windows.Forms.TextBox();
+            this.EncodeButt = new System.Windows.Forms.Button();
+            this.ErrorLbl = new System.Windows.Forms.Label();
+            this.progressBar = new System.Windows.Forms.ProgressBar();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.QualityLbl = new System.Windows.Forms.Label();
+            this.ffmpegCheck = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(13, 23);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(120, 28);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Select Folder";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // SourcePath
+            // 
+            this.SourcePath.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.SourcePath.Enabled = false;
+            this.SourcePath.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F);
+            this.SourcePath.Location = new System.Drawing.Point(138, 27);
+            this.SourcePath.Name = "SourcePath";
+            this.SourcePath.ReadOnly = true;
+            this.SourcePath.Size = new System.Drawing.Size(234, 20);
+            this.SourcePath.TabIndex = 2;
+            this.SourcePath.Text = "C:\\gmod\\sound\\weapons";
+            // 
+            // EncodeButt
+            // 
+            this.EncodeButt.Location = new System.Drawing.Point(252, 103);
+            this.EncodeButt.Name = "EncodeButt";
+            this.EncodeButt.Size = new System.Drawing.Size(120, 34);
+            this.EncodeButt.TabIndex = 5;
+            this.EncodeButt.Text = "Convert";
+            this.EncodeButt.UseVisualStyleBackColor = true;
+            this.EncodeButt.Click += new System.EventHandler(this.EncodeButt_Click);
+            // 
+            // ErrorLbl
+            // 
+            this.ErrorLbl.AutoSize = true;
+            this.ErrorLbl.BackColor = System.Drawing.SystemColors.Menu;
+            this.ErrorLbl.Location = new System.Drawing.Point(12, 169);
+            this.ErrorLbl.MaximumSize = new System.Drawing.Size(250, 40);
+            this.ErrorLbl.Name = "ErrorLbl";
+            this.ErrorLbl.Size = new System.Drawing.Size(0, 13);
+            this.ErrorLbl.TabIndex = 6;
+            // 
+            // progressBar
+            // 
+            this.progressBar.Location = new System.Drawing.Point(15, 103);
+            this.progressBar.Name = "progressBar";
+            this.progressBar.Size = new System.Drawing.Size(231, 34);
+            this.progressBar.TabIndex = 7;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.AllowDrop = true;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(264, 65);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(108, 21);
+            this.comboBox1.TabIndex = 10;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // QualityLbl
+            // 
+            this.QualityLbl.AutoSize = true;
+            this.QualityLbl.BackColor = System.Drawing.Color.Transparent;
+            this.QualityLbl.Location = new System.Drawing.Point(192, 68);
+            this.QualityLbl.Name = "QualityLbl";
+            this.QualityLbl.Size = new System.Drawing.Size(66, 13);
+            this.QualityLbl.TabIndex = 11;
+            this.QualityLbl.Text = "Sample rate:";
+            // 
+            // ffmpegCheck
+            // 
+            this.ffmpegCheck.AutoCheck = false;
+            this.ffmpegCheck.AutoSize = true;
+            this.ffmpegCheck.Location = new System.Drawing.Point(317, 169);
+            this.ffmpegCheck.Name = "ffmpegCheck";
+            this.ffmpegCheck.Size = new System.Drawing.Size(58, 17);
+            this.ffmpegCheck.TabIndex = 8;
+            this.ffmpegCheck.Text = "ffmpeg";
+            this.ffmpegCheck.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(384, 198);
+            this.Controls.Add(this.QualityLbl);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.ffmpegCheck);
+            this.Controls.Add(this.progressBar);
+            this.Controls.Add(this.ErrorLbl);
+            this.Controls.Add(this.EncodeButt);
+            this.Controls.Add(this.SourcePath);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Gmod Audio Encoder";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox SourcePath;
+        private System.Windows.Forms.Button EncodeButt;
+        private System.Windows.Forms.Label ErrorLbl;
+        private System.Windows.Forms.ProgressBar progressBar;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label QualityLbl;
+        private System.Windows.Forms.CheckBox ffmpegCheck;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Windows.Forms;
+
+namespace gmod_audio_converter
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            ffmpegCheck.Checked = File.Exists(@"ffmpeg.exe");
+
+            bool passedDependCheck = (File.Exists(@"ffmpeg.exe"));
+            { if (!passedDependCheck) { MessageBox.Show("You are missing one or more dependencies. This application will not function without them.", "Error! Missing Dependencies", MessageBoxButtons.OK, MessageBoxIcon.Exclamation); } }
+
+            System.Windows.Forms.ToolTip btnToolTip = new System.Windows.Forms.ToolTip();
+            btnToolTip.SetToolTip(this.button1, "Choose the folder where your audio files are stored.");
+
+            List<string> items = new List<string>() { "44100", "22050", "11025" };
+            comboBox1.DataSource = items;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog source = new FolderBrowserDialog();
+            if (DialogResult.OK == source.ShowDialog())
+            {
+                string path = source.SelectedPath;
+                System.Console.WriteLine(path);
+                SourcePath.Text = path;
+                SourcePath.Enabled = true;
+                SourcePath.BackColor = Color.White;
+            }
+        }
+
+        private void EncodeButt_Click(object sender, EventArgs e)
+        {
+            if ((SourcePath.Enabled))
+            {
+                progressBar.Maximum = 100;
+                progressBar.Value = 0;
+
+                int count = Directory.GetFiles(SourcePath.Text, "*.*").Length;
+                int iteration = 0;
+
+                if (Directory.Exists(SourcePath.Text))
+                {
+                    foreach (var file in Directory.GetFiles(SourcePath.Text, "*.*"))
+                    {
+                        Process process = new Process();    // Encode
+                        process.StartInfo.FileName = "ffmpeg";
+                        process.StartInfo.Arguments = "-y -i \u0022" + file + "\u0022 -c:a libmp3lame -ar " + comboBox1.SelectedItem + " \u0022" + SourcePath.Text + "\\" + Path.GetFileNameWithoutExtension(file) + ".mp3\u0022";
+                        process.StartInfo.UseShellExecute = false;
+                        process.StartInfo.CreateNoWindow = true;
+                        process.Start();
+                        iteration++;
+                        progressBar.Value += 100 / count;
+                        ErrorLbl.Text = "(" + iteration + " / " + count + ") Encoding " + Path.GetFileNameWithoutExtension(file);
+                        process.WaitForExit();
+                        File.Delete(file);
+                        System.Console.WriteLine(process.StartInfo.Arguments);
+                        if (iteration >= count)
+                        {
+                            ErrorLbl.Text = "Encoding complete. Converted " + iteration + " files to MP3 at " + SourcePath.Text + "Hz";
+                        }
+                    }
+                }
+            }
+            else
+            {
+                if (!SourcePath.Enabled)
+                {
+                    ErrorLbl.Text = "You did not select a source folder!";
+                }
+
+            }
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
